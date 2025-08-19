fce6673dac828aa6cd714193b4a9a5f0e71e2453
diogomoura97 <diogo.moura43@gmail.com>
2025-08-19 18:23:39 +0100

    Nível Aventureiro xadrez.c

--- xadrez.c
+++ xadrez.c
@@ -1,42 +1,42 @@
 #include <stdio.h>
 
 int main() {
-
-    printf("DESAFIO: NÍVEL NOVATO \n");
+    
+    printf("DESAFIO: NÍVEL AVENTUREIRO \n");
     printf(" \n ");
 
-    printf("Movento a Torre 5 casas para a direita com loop for \n");
+    printf("Movento o cavalo 2 casas para baixo e uma casa para a esquerda com loop for aninhado \n");
     printf(" \n ");
+ 
+    for (int repeticoesLoop=0; repeticoesLoop<1; repeticoesLoop++){ //Número de repetições do loop
 
-    for(int movimentotorre = 0;movimentotorre<5;movimentotorre++){
-        printf("Direita-> %dª casa \n",movimentotorre+1);
+      for (int repeticoesCondicao = 0; repeticoesCondicao<2; repeticoesCondicao++) //Número de repetições da condição, para que o loop principal volte a repetir (caso haja mais de uma repetição)
+      {
+         printf("Baixo\n");
+      }
 
-    }
+      printf("Esquerda\n");
+   }
+
+   //----------------------------------------------------------------------------------------------
+
+   printf("Movento o cavalo 2 casas cima e uma casa para a direita com loop while aninhado \n");
+   printf(" \n ");
+
+   int i = 0;
    
-
-    //----------------------------------------------------------------------------------------------
-
-     printf("Movento o Bispo 5 casas para a diagonal superior direita com loop while \n");
-     printf(" \n ");
-
-     int movimentobispo = 0;
-     while (movimentobispo<5)
-     {
-        printf("Cima,Direita-> %dª casa \n",movimentobispo+1);
-        movimentobispo++;
-     }
    
-     //----------------------------------------------------------------------------------------------
+    while (i<1) { //O primeiro loop while fará o loop se repetir uma vez, pois a condição i é de 0 até 1
+       int j = 0;
 
-     printf("Movento a Rainha 5 casas para a direita com loop do-while \n");
-     printf(" \n ");
+       while (j<2){ //O segundo loop while irá imprimir a mensagem 2 vezes, pois a condição j é de 0 até 2
 
-     int movimentorainha = 0;
-     do
-     {
-        printf("Esquerda-> %dª casa \n",movimentorainha+1);
-        movimentorainha++;
-     } while (movimentorainha<8);
+      printf("Cima \n");
+         j++; // Incremento para que o valor inicial de j receba +1
+      }
+      printf("Direta \n"); //Depois que o loop for concluído e rodar uma vez, a mensagem fora do loop irá aparecer por padrão
+      i++; // Incremento para que o valor inicial de i receba +1
+   }
 
     return 0;
 }
